fc6cd81262ba949fca669ba6c273c94f35ae04e7
elizatu <eliza.turco@gmail.com>
2018-07-26 20:52:37 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -2,13 +2,12 @@
 # Challenge 1: Fill this string in with your favorite word
 
 your_favorite_word = "bananas"
-your_favorite_word.
 
 puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-"BANANAS" = your_favorite_word # Put a . after your_favorite word and use a String method.
+"bananas" = your_favorite_word # Put a . after your_favorite word and use a String method.
 
 puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
